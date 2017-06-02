b3f9ed94c433d0f8ab81d43a2a5860c20c9de6eb
Alexander Reimann <A.Reimann@yujinrobot.com>
2017-06-02 16:42:21 +0900

    Add dynamic reconfigure, debug param

--- include/plane_calibration/plane_calibration_nodelet.hpp
+++ include/plane_calibration/plane_calibration_nodelet.hpp
@@ -1,7 +1,10 @@
 #ifndef plane_calibration_SRC_PLANE_CALIBRATION_NODELET_HPP_
 #define plane_calibration_SRC_PLANE_CALIBRATION_NODELET_HPP_
 
+#include <atomic>
 #include <nodelet/nodelet.h>
+#include <dynamic_reconfigure/server.h>
+#include <plane_calibration/PlaneCalibrationConfig.h>
 
 
 namespace plane_calibration
@@ -10,10 +13,16 @@ namespace plane_calibration
 class PlaneCalibrationNodelet : public nodelet::Nodelet
 {
 public:
+  PlaneCalibrationNodelet();
+
   virtual void onInit();
 
 protected:
+  virtual void reconfigureCB(PlaneCalibrationConfig &config, uint32_t level);
 
+  std::shared_ptr<dynamic_reconfigure::Server<PlaneCalibrationConfig> > reconfigure_server_;
+
+  std::atomic<bool> debug_;
 };
 
 } /* end namespace */
--- src/plane_calibration_nodelet.cpp
+++ src/plane_calibration_nodelet.cpp
@@ -4,11 +4,27 @@
 
 namespace plane_calibration
 {
+  PlaneCalibrationNodelet::PlaneCalibrationNodelet()
+  {
+    debug_ = false;
+  }
+
   void PlaneCalibrationNodelet::onInit()
   {
       ROS_INFO("[PlaneCalibrationNodelet]: Initializing");
 
+      reconfigure_server_ = std::shared_ptr<dynamic_reconfigure::Server<PlaneCalibrationConfig> >(
+          new dynamic_reconfigure::Server<PlaneCalibrationConfig>());
+      dynamic_reconfigure::Server<PlaneCalibrationConfig>::CallbackType reconfigure_cb = boost::bind(&PlaneCalibrationNodelet::reconfigureCB,
+                                                                                                this, _1, _2);
+      reconfigure_server_->setCallback(reconfigure_cb);
+
       ROS_INFO("[PlaneCalibrationNodelet]: Initialization finished");
+  }
+
+  void PlaneCalibrationNodelet::reconfigureCB(PlaneCalibrationConfig &config, uint32_t level)
+  {
+    debug_ = config.debug;
   }
 } /* end namespace */
 
